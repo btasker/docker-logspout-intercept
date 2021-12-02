cd2bff6d28b97db5fbdc2350097e6dc006cfd0ea
B Tasker <88340935+btasker@users.noreply.github.com>
2021-12-02 14:09:09 +0000

    Add ability to look up details from container info

--- app/capture_and_print.py
+++ app/capture_and_print.py
@@ -4,21 +4,43 @@
 # Copyright (c) 2021 B Tasker
 #
 
+import docker
 import sys
 from scapy.all import *
 
 
-def pkt_callback(pkt):
+def prnt_line(pkt):
     ''' Callback function to dump out the payload
 
     It might seem a bit redundant to pass the payload into `bytes` and
     immediately decode it. The reason is that it'll otherwise technically
     be raw, but print out as if it were bytes
     '''
-
-   p = bytes(pkt[UDP].payload)
-   print(p.decode())
-
+    p = bytes(pkt[UDP].payload)
+    print(p.decode(), end='')
 
 
-sniff(iface='eth0', filter=f'udp port {sys.argv[1]}', prn=pkt_callback, store=0)
+def get_syslog_port_from_container(cont):
+
+    dockerClient = docker.from_env()
+    client = docker.APIClient(base_url='unix://var/run/docker.sock')
+
+    logconf = client.inspect_container(cont)['HostConfig']['LogConfig']
+    if logconf['Type'] != "syslog":
+        return False
+
+    # Return the port
+    return logconf['Config']['syslog-address'].split(":")[-1]
+
+
+port=sys.argv[1]
+if not port.isnumeric():
+    # We've been given a docker container name instead
+    port = get_syslog_port_from_container(sys.argv[1])
+    if not port:
+        print("Error - container {sys.argv[1]} is not using Syslog")
+        sys.exit(1)
+
+
+
+sniff(iface='eth0', filter=f'udp port {port}', prn=prnt_line, store=0)
